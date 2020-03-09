26594b43f6df7e4dcd407fd0ee8aa67cb14e4ff0
bartc <bartcramba@gmail.com>
2020-03-09 11:36:15 +0200

    Adding the project with 2 Unit tests

--- src/test/java/org/example/AppTest.java
+++ src/test/java/org/example/AppTest.java
@@ -2,8 +2,11 @@ package org.example;
 
 import static org.junit.Assert.assertTrue;
 
+import domain.Student;
 import org.junit.Assert;
 import org.junit.Test;
+import repository.StudentRepository;
+import validation.StudentValidator;
 
 /**
  * Unit test for simple App.
@@ -17,5 +20,26 @@ public class AppTest
     public void shouldAnswerWithTrue()
     {
         Assert.assertTrue( true );
+    }
+
+    @Test
+    public void testAddStudent1() {
+
+        StudentValidator studentValidator = new StudentValidator();
+        StudentRepository repository = new StudentRepository(studentValidator);
+        Student student = new Student("1234","Cramba", 932);
+        repository.save(student);
+
+        Assert.assertNotNull(repository.findOne(student.getID()));
+    }
+
+    @Test
+    public void testAddStudent2() {
+
+        StudentValidator studentValidator = new StudentValidator();
+        StudentRepository repository = new StudentRepository(studentValidator);
+        Student student = new Student("1234","Flavius", 933);
+
+        Assert.assertNull(repository.findOne(student.getID()));
     }
 }
