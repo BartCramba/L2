a335b78c4dcbe6056e28fe346749d3993561e0b4
bartc <bartcramba@gmail.com>
2020-03-23 11:23:25 +0200

    Adding the project with 2 Unit tests

--- src/test/java/org/example/AppTest.java
+++ src/test/java/org/example/AppTest.java
@@ -19,32 +19,32 @@ public class AppTest
     /**
      * Rigorous Test :-)
      */
-//    @Test
-//    public void shouldAnswerWithTrue()
-//    {
-//        Assert.assertTrue( true );
-//    }
-//
-//    @Test
-//    public void testAddStudent1() {
-//
-//        StudentValidator studentValidator = new StudentValidator();
-//        StudentRepository repository = new StudentRepository(studentValidator);
-//        Student student = new Student("1234","Cramba", 932);
-//        repository.save(student);
-//
-//        Assert.assertNotNull(repository.findOne(student.getID()));
-//    }
-//
-//    @Test
-//    public void testAddStudent2() {
-//
-//        StudentValidator studentValidator = new StudentValidator();
-//        StudentRepository repository = new StudentRepository(studentValidator);
-//        Student student = new Student("1234","Flavius", 933);
-//
-//        Assert.assertNull(repository.findOne(student.getID()));
-//    }
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        Assert.assertTrue( true );
+    }
+
+    @Test
+    public void testAddStudent1() {
+
+        StudentValidator studentValidator = new StudentValidator();
+        StudentRepository repository = new StudentRepository(studentValidator);
+        Student student = new Student("1234","Cramba", 932);
+        repository.save(student);
+
+        Assert.assertNotNull(repository.findOne(student.getID()));
+    }
+
+    @Test
+    public void testAddStudent2() {
+
+        StudentValidator studentValidator = new StudentValidator();
+        StudentRepository repository = new StudentRepository(studentValidator);
+        Student student = new Student("1234","Flavius", 933);
+
+        Assert.assertNull(repository.findOne(student.getID()));
+    }
 
     @Test
     public void testAddAssignment1(){
